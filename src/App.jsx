803afbc4b695e145e41b12fb0fd6bbc7ab1f8c95
yi7.liu <yi7.liu@symbio.com>
2023-03-04 17:11:39 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "@code-hike/mdx/styles"
+import Content from './hello.mdx'
+function App() {
+ 
+  return (
+    <div className="App">
+       <Content/>
+    </div>
+  )
+}
+
+export default App
